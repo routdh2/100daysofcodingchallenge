3fd955cda4cbd78430a8d426c392e8debeb57e17
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-05 21:48:55 +0530

    contains duplicate program in python

---
+++ Day7/contains_duplicate.py
@@ -0,0 +1,4 @@
+'''Problem Statement: https://leetcode.com/problems/contains-duplicate/ '''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums))!=len(nums)
