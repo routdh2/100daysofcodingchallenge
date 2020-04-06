52418cc5b75d46ce19cab756178adbe0f6be458a
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-06 06:18:45 +0530

    changed default value to zero

--- Day8/unique_occurences.py
+++ Day8/unique_occurences.py
@@ -3,7 +3,7 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         dic={}
         for i in arr:
-            dic[i]=1+dic.get(i,1)
+            dic[i]=1+dic.get(i,0)
         if len(dic.values())==len(set(dic.values())):
             return True
         else:
