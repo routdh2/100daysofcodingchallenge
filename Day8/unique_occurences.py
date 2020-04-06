717808c6307a67d6684db6781de4566f50d0b1fb
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-06 05:50:57 +0530

    Unique occurences code in python

---
+++ Day8/unique_occurences.py
@@ -0,0 +1,10 @@
+'''Problem Statement: https://leetcode.com/problems/unique-number-of-occurrences'''
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        dic={}
+        for i in arr:
+            dic[i]=1+dic.get(i,1)
+        if len(dic.values())==len(set(dic.values())):
+            return True
+        else:
+            return False
