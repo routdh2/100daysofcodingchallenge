46d921a823b1efb9249c196056567533a2e584b9
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-06 16:11:13 +0530

    occurences after bigram code in python

---
+++ Day9/occurences_after_bigram.py
@@ -0,0 +1,11 @@
+'''Problem Statement: https://leetcode.com/problems/occurrences-after-bigram'''
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words=text.split()
+        result=[]
+        for i in range(len(words)-2):
+            if words[i]==first and words[i+1]==second:
+                result.append(words[i+2])
+        return result
+        
+        
