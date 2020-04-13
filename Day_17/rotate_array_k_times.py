a99ddca4f15c86ed4b629f6c820ad336f2c67039
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-13 07:24:02 +0530

    Rotate array by k times code in python

---
+++ Day_17/rotate_array_k_times.py
@@ -0,0 +1,8 @@
+'''Problem Statement: https://leetcode.com/problems/rotate-array'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0,nums.pop())
