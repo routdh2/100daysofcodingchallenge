d67fc8be31a9604f91d654f6aae4ac56e0b50aa1
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-14 06:51:18 +0530

    sum of two ints using bit manipulation code in Java

---
+++ Day_19/sum_of_two_ints_bit_manipulation.java
@@ -0,0 +1,15 @@
+/**
+* Problem Statement: https://leetcode.com/problems/sum-of-two-integers
+*/
+class Solution {
+    public int getSum(int a, int b) {
+        int carry;
+        while(b!=0) {
+            carry=a&b;
+            a=a^b;
+            b=carry<<1;
+        }
+        return a;
+        
+    }
+}
