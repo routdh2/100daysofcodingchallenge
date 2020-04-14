85f3cd73bdcfc594e3516f97e423661c8f6b61fe
Dhananjay Rout <routvoice.1993@gmail.com>
2020-04-14 06:58:44 +0530

    Added comments

--- Day_19/sum_of_two_ints_bit_manipulation.java
+++ Day_19/sum_of_two_ints_bit_manipulation.java
@@ -4,10 +4,10 @@
 class Solution {
     public int getSum(int a, int b) {
         int carry;
-        while(b!=0) {
-            carry=a&b;
-            a=a^b;
-            b=carry<<1;
+        while(b!=0) { //While carry!=0 -> meaning loop over till there is carry
+            carry=a&b; //a&b generates carry
+            a=a^b;//a^b does the addition
+            b=carry<<1;//we need to shift the carry left by 1 bit so that in next iteration it can be added to a
         }
         return a;
         
